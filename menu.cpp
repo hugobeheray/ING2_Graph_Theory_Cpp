b15c0678d17d3e585a25123d3585b0a516d5bd7c
Hugo <hbeheray@gmail.com>
2020-04-27 13:21:55 +0200

    ajout couleur

--- menu.cpp
+++ menu.cpp
@@ -3,9 +3,22 @@
 #include "Sommet.h"
 #include <vector>
 #include "svgfile.h"
+#include "windows.h"
+
+void couleurverte()
+{
+    HANDLE couleurcin=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(couleurcin, 10);
+}
+void couleurcyan()
+{
+    HANDLE couleurcin=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(couleurcin, 3);
+}
 
 void menu()
 {
+    couleurcyan();
     int choix;
     while(choix!=5)
     {
@@ -13,7 +26,9 @@ void menu()
         {
             std::cout << "Bienvenue" << std::endl << "Que voulez vous faire?" << std::endl << "1.Charger un graphe" << std::endl << "2.Changer le systeme de ponderation d'un graphe" << std::endl <<
                       "3. Calculer, afficher et sauvegarder les differents indices de centralite" << std::endl << "4.Tester la vulnerabilite du graphe" << std::endl << "5.Quitter" << std::endl;
+            couleurverte();
             std::cin >> choix;
+            couleurcyan();
         }
         while(choix < 1 || choix > 5);
         switch(choix)
