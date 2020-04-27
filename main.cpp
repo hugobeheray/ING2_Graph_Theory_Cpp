038072dc3a77f424ec26cc79e0fc3bb3ad0df634
Corentin Rozenblum <corentin.rozenblum@edu.ece.fr>
2020-04-27 16:14:26 +0200

    Nouveaux graphes

--- main.cpp
+++ main.cpp
@@ -12,11 +12,10 @@ int main(int argc, char *argv[])
     std::string nomfichier;
     //std::cout << "Veuillez indiquer le nom du fichier a charger" << std::endl;
     //std::cin >> nomfichier;
-    Graphe graphe("graphe_etoile1_topo.txt");
+    Graphe graphe("graphe_cycle4_topo.txt");
     graphe.afficher();
     Svgfile svgfile;
     graphe.dessiner(&svgfile);
-
     return 0;
 }
 
