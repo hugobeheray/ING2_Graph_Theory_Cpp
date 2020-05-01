47c6bf3def60de4f197840bdb074915b71e05f88
Hugo <hbeheray@gmail.com>
2020-05-01 23:36:00 +0200

    nouveau menu

--- menu.cpp
+++ menu.cpp
@@ -43,6 +43,7 @@ void menu()
     int choix;
     std::vector<float> tabresultats;
     std::string nomfichierpoids,nomfichiertopo;
+    int choixnomfichierpoids;
 
 
     Graphe graphe;
@@ -64,19 +65,30 @@ void menu()
         switch(choix)
         {
         case 1:
-    std::cout << "Veuillez indiquer le nom du fichier de poids a charger" << std::endl;
-    couleurverte();
-    std::cin >> nomfichierpoids;
-    couleurcyan();
-    std::cout << "Veuillez indiquer le nom du fichier a charger" << std::endl;
-    couleurverte();
-    std::cin >> nomfichiertopo;
-    couleurcyan();
-            std::cout << nomfichierpoids << nomfichiertopo << std::endl;
-             graphe.chargementPoids(nomfichierpoids);
-          graphe.chargementTopo(nomfichiertopo);
-            std::cout << nomfichierpoids << nomfichiertopo << std::endl;
+   couleurcyan();
+            std::cout << "Veuillez indiquer le nom du fichier a charger\n\n\n" << std::endl;
+            std::cout <<"\t 1- Graphe Etoile 1\n\t 2- Graphe Etoile 3\n\t 3- Graphe Cycle 4" << std::endl;
+            std::cout <<"\t 4- Graphe Cycle 5\n\t 5- Graphe Amerique du Sud\n\t 6- Graphe Metro Parisien" << std::endl;
+            couleurverte();
+            std::cin >> choixnomfichierpoids;
+            while(choixnomfichierpoids<1 || choixnomfichierpoids>6)
+            {
+                couleurcyan();
+                std::cout << "Erreur de saisie. Veuillez saisir un choix valide." << std::endl;
+                couleurverte();
+                std::cin >> choixnomfichierpoids;
+            }
+            if(choixnomfichierpoids==1) {nomfichierpoids="graphe_etoile1_topo_poids.txt";nomfichiertopo="graphe_etoile1_topo.txt";}
+            if(choixnomfichierpoids==2) {nomfichierpoids="graphe_etoile3_topo_poids.txt";nomfichiertopo="graphe_etoile3_topo.txt";}
+            if(choixnomfichierpoids==3) {nomfichierpoids="graphe_cycle4_topo_poids.txt";nomfichiertopo="graphe_cycle4_topo.txt";}
+            if(choixnomfichierpoids==4) {nomfichierpoids="graphe_cycle5_topo_poids.txt";nomfichiertopo="graphe_cycle5_topo.txt";}
+            if(choixnomfichierpoids==5) {nomfichierpoids="graphe_ameriquedusud_topo_poids.txt";nomfichiertopo="graphe_ameriquedusud_topo.txt";}
+            if(choixnomfichierpoids==6) {nomfichierpoids="graphe_metro_paris_topo_poids.txt";nomfichiertopo="graphe_metro_paris_topo.txt";}
+            couleurcyan();
+            graphe.chargementPoids(nomfichierpoids);
+            graphe.chargementTopo(nomfichiertopo);
             graphe.afficher();
+            break;
             break;
         case 2:
             graphe.suppression_arete(nomfichierpoids,nomfichiertopo,graphe);
